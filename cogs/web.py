9a48d46059bea8c093b7d0896b0233301a48fe0f
Enitoxy <85189523+Enitoxy@users.noreply.github.com>
2025-03-08 01:54:27 +0200

    Rename apis.py to web.py (found it better)

--- cogs/web.py
+++ cogs/web.py
@@ -13,7 +13,7 @@ from discord import Embed, Interaction, app_commands
 from discord.ext import commands
 
 
-class APIs(commands.Cog):
+class Web(commands.Cog):
     def __init__(self, bot: commands.AutoShardedBot):
         self.bot = bot
 
@@ -38,4 +38,4 @@ class APIs(commands.Cog):
 
 
 async def setup(bot: commands.AutoShardedBot):
-    await bot.add_cog(APIs(bot))
+    await bot.add_cog(Web(bot))
