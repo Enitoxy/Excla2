9b774df3511be378527f852ce01cd3c77bdb7812
Enitoxy <85189523+Enitoxy@users.noreply.github.com>
2025-03-08 16:01:01 +0200

    Add database functionality (database implementation in next commit)

--- cogs/economy.py
+++ cogs/economy.py
@@ -13,6 +13,7 @@ from discord import Embed, Interaction, app_commands
 from discord.ext import commands
 
 from data.fish import fishes
+from utils import db
 
 
 class Economy(commands.Cog):
@@ -38,13 +39,30 @@ class Economy(commands.Cog):
         else:
             return f"<:{fish}:{emoji_id}>"
 
+    async def new_inventory(self, user_id: int):
+        data = {
+            "user_id": user_id,
+            "bits": 0,
+        }
+        await db.inventory.insert_one(data)
+
+    async def add_one_fish(self, interaction: Interaction, fish: str):
+        query = {"user_id": interaction.user.id}
+        user_inventory = await db.inventory.find_one(query)
+
+        if user_inventory is None:
+            await self.new_inventory(interaction.user.id)
+
+        data = {"$inc": {fish: 1}}
+        await db.inventory.update_one(query, data)
+
     @app_commands.command(name="fish")
     async def fish(self, interaction: Interaction):
         fish = await self.get_fish()
         fish_name = fishes[fish]["name"]
         fish_value = fishes[fish]["value"]
 
-        # Database entry before the embed
+        await self.add_one_fish(interaction, fish)
 
         emoji = await self.get_emoji(fish)
 
