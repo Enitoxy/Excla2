62f73f7fa7f4af98121cfbfc838c1cf79dc446ea
Enitoxy <85189523+Enitoxy@users.noreply.github.com>
2025-03-08 16:30:17 +0200

    Simple database connector and example environment variables

---
+++ utils/db.py
@@ -0,0 +1,24 @@
+import os
+from glob import glob
+
+from pymongo import AsyncMongoClient
+
+MONGODB = os.getenv("MONGODB")
+
+# Place a single X509 certificate at the root of the project, in certs/
+cert = glob("./certs/X509-cert*.pem")
+client = AsyncMongoClient(
+    f"mongodb+srv://{MONGODB}",
+    authSource="$external",
+    authMechanism="MONGODB-X509",
+    retryWrites=True,
+    w="majority",
+    tls=True,
+    tlsCertificateKeyFile=cert[0],
+)
+
+# From the client, we get a database by its name
+db = client["excla2"]
+
+# Then we get a collection, in this case it's named inventory
+inventory = db["inventory"]
