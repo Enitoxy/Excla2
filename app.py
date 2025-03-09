68d357bf10485b22d51036157e53f0450587c162
Enitoxy <85189523+Enitoxy@users.noreply.github.com>
2025-03-09 18:46:06 +0200

    Change config port from string to integer

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ templates = Jinja2Templates(directory="www/templates")
 config = uvicorn.Config(
     app,
     host="0.0.0.0",
-    port="8000",
+    port=8000,
     log_level="info",
 )
 server = uvicorn.Server(config)
